3d16b1eff5f5f5d5e6859a2afb8e1ae1b2a45bea
kabs93 <kabileish@gmail.com>
2021-10-15 07:19:42 +0800

    initializing repo

---
+++ corroserv_inventory/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from corroserv_inventory.users.models import User
+from corroserv_inventory.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
