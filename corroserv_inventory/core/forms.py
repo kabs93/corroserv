87513faf622cbaa508d2f2224f30ff7232b75d57
kabs93 <kabileish@gmail.com>
2021-10-27 09:00:53 +0800

    error handling for Movement Confirm pages

--- corroserv_inventory/core/forms.py
+++ corroserv_inventory/core/forms.py
@@ -28,3 +28,32 @@ class InboundForm(forms.Form):
     #         self.add_error("instructions", "Should not end with a full stop")
 
     #     return instructions
+
+
+class OutboundForm(forms.Form):
+    location = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            },
+        ),
+    )
+    quantity = forms.IntegerField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            },
+        ),
+    )
+    # def clean_instructions(self):
+    #     instructions = self.cleaned_data["instructions"]
+    #     if not instructions:
+    #         return instructions
+
+    #     if not instructions[0].isupper():
+    #         self.add_error("instructions", "Should start with an uppercase letter")
+
+    #     if instructions.endswith("."):
+    #         self.add_error("instructions", "Should not end with a full stop")
+
+    #     return instructions
--- corroserv_inventory/core/views.py
+++ corroserv_inventory/core/views.py
@@ -3,8 +3,8 @@ from django.db import transaction
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .forms import InboundForm
-from .models import Inventory, Location
+from .forms import InboundForm, OutboundForm
+from .models import Inventory, Location, Task, TaskStatus, TaskType
 
 
 @login_required
@@ -46,26 +46,54 @@ def movement_confirm(
         inventory_listing,
     ) = Inventory.objects.get_details_for_item(inventory_item_id)
 
-    if movement_type == "inbound":
+    if movement_type == "Inbound":
         form = InboundForm
+    elif movement_type == "Outbound":
+        form = OutboundForm
     else:
         form = None
+    location_input_error = None
 
     if request.method == "POST":
         form = form(request.POST)
         if form.is_valid():
-            quantity_input = form.cleaned_data["quantity"]
+            quantity_input = (
+                int(form.cleaned_data["quantity"])
+                if movement_type == "Inbound"
+                else -int(form.cleaned_data["quantity"])
+            )
             try:
                 location_input = Location.objects.get(
                     name=form.cleaned_data["location"]
                 )
                 location_input_error = ""
                 with transaction.atomic():
-                    pass
-                    #####################################################################
-                    # SETUP TRANSACTIONS FOR MOVEMENT, WILL TOUCH INVENTORY, TASK etc. MODELS
-                    #####################################################################
+                    task_obj = Task.objects.create(
+                        item=item,
+                        location=location_input,
+                        type=TaskType.objects.get(name=movement_type),
+                    )
+                    TaskStatus.objects.create(task=task_obj, status="CTD")
 
+                    try:
+                        inventory_list_item = inventory_listing.get(
+                            location=location_input
+                        )
+                        inventory_list_item.quantity = (
+                            inventory_list_item.quantity + quantity_input
+                        )
+                        inventory_list_item.save()
+                    except Inventory.DoesNotExist:
+                        if movement_type == "Inbound":
+                            Inventory.objects.create(
+                                item=item,
+                                location=location_input,
+                                quantity=quantity_input,
+                            )
+                        elif movement_type == "Outbound":
+                            location_input_error = (
+                                "Inventory location does not exist for this item"
+                            )
             except Location.DoesNotExist:
                 location_input = None
                 location_input_error = "Location does not exist"
